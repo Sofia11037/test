b6480b256b4c0c77e34aa2f330b4ab5824e9f54e
Sofia11037 <113290148+Sofia11037@users.noreply.github.com>
2026-01-09 23:35:14 +0100

    Update Instagram.jsx

--- src/Pages/Local/Instagram/Instagram.jsx
+++ src/Pages/Local/Instagram/Instagram.jsx
@@ -12,12 +12,12 @@ export default function Instagram() {
                 <div className="for-center">
                 <div className="photos">
                     <div className="photo first">
-                        <img src="/src/assets/images/instagram/photo1.png" alt="" />
-                        <img className='icon' src="/src/assets/images/instagram/iconInst.png" alt="" />
+                        <img src="./src/assets/images/instagram/photo1.png" alt="" />
+                        <img className='icon' src="./src/assets/images/instagram/iconInst.png" alt="" />
                     </div>
-                    <div className="photo"><img src="/src/assets/images/instagram/photo2.png" alt="" /></div>
-                    <div className="photo"><img src="/src/assets/images/instagram/photo3.png" alt="" /></div>
-                    <div className="photo"><img src="/src/assets/images/instagram/photo4.png" alt="" /></div>
+                    <div className="photo"><img src="./src/assets/images/instagram/photo2.png" alt="" /></div>
+                    <div className="photo"><img src="./src/assets/images/instagram/photo3.png" alt="" /></div>
+                    <div className="photo"><img src="./src/assets/images/instagram/photo4.png" alt="" /></div>
                 </div>
                 </div>
 
@@ -25,4 +25,5 @@ export default function Instagram() {
             </div>
         </div>
     );
+
   }
